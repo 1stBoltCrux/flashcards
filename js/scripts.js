02718462990a9542ce305e9f786366eab98a6201
Garnett Puett and Ted Leary <garnett@>
2018-03-20 11:01:03 -0700

    Add three initial flashcard toggles.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#js-hid, #js-show").click(function(){
+    $("#js-show").toggle();
+    $("#js-hid").toggle();
+  });
+  $("#op-hid, #op-show").click(function(){
+    $("#op-show").toggle();
+    $("#op-hid").toggle();
+  });
+  $("#var-hid, #var-show").click(function(){
+    $("#var-show").toggle();
+    $("#var-hid").toggle();
+  });
+});
